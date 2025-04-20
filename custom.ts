3ed46ad50367437e429731c00406ae6e9958d5c3
Dmitriy Antipov <aston_bonny@mail.ru>
2025-04-20 19:47:19 +0300

    Обновить custom.ts

--- custom.ts
+++ custom.ts
@@ -30,6 +30,13 @@ namespace custom {
         return mostFrequentNum;
     }
 
+    export function calculateMedian(numbers: number[]): number {
+        if (numbers.length === 0) return null;
+        numbers = numbers.sort((a, b) => a - b);
+        const half = Math.floor(numbers.length / 2);
+        return numbers.length % 2 ? numbers[half] : (numbers[half - 1] + numbers[half]) / 2;
+    }
+
 }
 
 namespace music {
