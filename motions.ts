fcc5530d7b71c54f07fe5bf106f0a25f7f743b05
Dmitriy Antipov <aston_bonny@mail.ru>
2025-06-18 17:59:23 +0300

    Обновить motions.ts

--- motions.ts
+++ motions.ts
@@ -1,6 +1,6 @@
 namespace motions {
 
-    export let minPwrAtEndMovement = 5; // Минимальная мощность при завершении движения
+    export let minPwrAtEndMovement = 10; // Минимальная мощность при завершении движения
 
     // Функция, которая выполняет действие после цикла с движением
     export function actionAfterMotion(actionAfterMotion: AfterMotion | MotionBraking, speed?: number) { // 
