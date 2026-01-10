22cb6a0acaea93103febbb9de765bf78768a9b34
Dmitriy Antipov <aston_bonny@mail.ru>
2026-01-10 20:53:00 +0300

    Обновить motors.ts

--- motors.ts
+++ motors.ts
@@ -30,22 +30,22 @@ namespace motors {
     //% group="Управление положением"
     export function setPosition(motor: motors.Motor, pos: number, braking: Braking, params?: params.MotorRegulator, debug: boolean = false) {
         if (params) {
-            if (params.vMax >= 0) regMotorMaxV = params.vMax;
+            if (params.vMax >= 0) regMotorMaxV = Math.abs(params.vMax);
             if (params.Kp >= 0) regMotorKp = params.Kp;
             if (params.Ki >= 0) regMotorKi = params.Ki;
             if (params.Kd >= 0) regMotorKd = params.Kd;
             if (params.Kf >= 0) regMotorKf = params.Kf;
-            if (params.errorThreshold >= 0) errorThreshold = params.errorThreshold;
-            if (params.vMinThreshold >= 0) vMinThreshold = params.vMinThreshold;
-            if (params.timeOut >= 0) regMotorTimeOut = params.timeOut;
+            if (params.errorThreshold >= 0) errorThreshold = Math.abs(params.errorThreshold);
+            if (params.vMinThreshold >= 0) vMinThreshold = Math.abs(params.vMinThreshold);
+            if (params.timeOut >= 0) regMotorTimeOut = Math.abs(params.timeOut);
         }
 
         motor.setBrake(braking == Braking.Hold); // Установка удерживания мотором позиции
 
         pidRegMotor.setGains(regMotorKp, regMotorKi, regMotorKd); // Установка коэффицентов ПИД регулятора
         pidRegMotor.setDerivativeFilter(regMotorKf); // Установить фильтр дифференциального регулятора
-        pidRegMotor.setControlSaturation(-500, 500); // Установка интервала ПИД регулятора
-        // pidRegMotor.setPoint(pos); // Установить нулевую уставку регулятору
+        pidRegMotor.setControlSaturation(-360, 360); // Установка интервала ПИД регулятора
+        pidRegMotor.setPoint(pos); // Установить нулевую уставку регулятору
         pidRegMotor.reset(); // Сброс ПИД регулятора
 
         if (debug) console.log(`Start motors.setPosition(${pos})`);
@@ -61,8 +61,7 @@ namespace motors {
             const currentAngle = motor.angle();
             const error = pos - currentAngle; // Расчитываем ошибку положения
             if (Math.abs(error) <= errorThreshold && Math.abs(motor.speed()) <= vMinThreshold) break; // Угол был достигнут
-            // pidRegMotor.setPoint(error); // Передать ошибку регулятору
-            let u = pidRegMotor.compute(dt == 0 ? 1 : dt, -error); // Управляющее воздействие
+            let u = pidRegMotor.compute(dt == 0 ? 1 : dt, currentAngle); // Управляющее воздействие
             u = Math.constrain(u, -regMotorMaxV, regMotorMaxV); // Ограничиваем
             motor.run(u); // Установить мотору управляющее воздействие
             if (debug && currTime - prevDebugPrintTime >= 10) {
@@ -70,11 +69,8 @@ namespace motors {
                 // brick.printString(`angle: ${motor.angle()}`, 1);
                 // brick.printString(`error: ${error}`, 2);
                 // brick.printString(`U: ${U}`, 3);
-                // console.log(`millis: ${control.millis() - startTime}, angle: ${currentAngle}, error: ${error}, U: ${U}`);
-                control.runInParallel(function() {
-                    console.log(`${currentAngle}, ${error}, ${u}`);
-                    prevDebugPrintTime = control.millis();
-                })
+                console.log(`currAngle: ${currentAngle}, error: ${error}, u: ${u}`);
+                prevDebugPrintTime = control.millis();
             }
             control.pauseUntilTime(currTime, 5); // Ожидание выполнения цикла за нужную частоту
         }
