57bc5c0b865b5167ce3e90a3393e9c518a8ec917
Dmitriy Antipov <aston_bonny@mail.ru>
2026-06-08 19:40:22 +0300

    Обновить math.ts

--- math.ts
+++ math.ts
@@ -10,17 +10,17 @@ namespace Math {
         return ((degrees * chassis.getBaseLength()) / chassis.getWheelDiametr()) * (motors.cpr / 360);
     }
 
-    // This define returns the smallest of the three numbers
+    // Вспомогательная функция, которая возвращает наименьшее из трех чисел
     export function min3(a: number, b: number, c: number): number {
         return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
     }
 
-    // This function returns the biggest of the three numbers
+    // Вспомогательная функция, которая возвращает наибольшее из трех чисел
     export function max3(a: number, b: number, c: number): number {
         return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
     }
 
-    // Returns x if it is between min and max. If outside the range, it returns min or max.
+    // Возвращает значение x, если оно находится в диапазоне от min до max. Если значение выходит за пределы диапазона, возвращает min или max
     export function clip(a: number, b: number, c: number): number {
         return Math.min(c, Math.max(b, a));
     }
